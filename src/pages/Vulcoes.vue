4aacce7af6778097eda04e9f389535ac0c7401a0
Mickael Souza <mickael.souza.if@gmail.com>
2020-03-04 03:26:48 -0300

    Fazendo upload de imagens

--- src/pages/Vulcoes.vue
+++ src/pages/Vulcoes.vue
@@ -14,7 +14,7 @@
                         </div>
                         <div class="card-image">
                             <figure class="image is-4by3">
-                                  <img :src="`/img/${volcano.img}`">
+                                  <img :src="`http://192.168.0.110:3000/${volcano.img}`">
                             </figure>
                         </div>
                       
@@ -41,7 +41,7 @@
                         <button class="delete" aria-label="close" @click="closeAddVolcanoes"></button>
                     </header>
                     <section class="modal-card-body is-large">
-                         <article v-show="hasError" class="message is-danger">
+                        <article v-show="hasError" class="message is-danger">
                             <div class="message-header">
                                 <p>Erro</p>
                                 <button class="delete" aria-label="delete" @click="hasError = false"></button>
@@ -50,32 +50,53 @@
                                 {{ errorMessage }}
                             </div>
                         </article>
-                        <div class="field">
-                            <label class="label">Nome</label>
-                            <div class="control">
-                                <input class="input" type="text" placeholder="Nome" v-model="fields.name">
-                            </div>
-                        </div>
-                        <div class="field">
-                            <label class="label">Descrição</label>
-                            <div class="control">
-                                <textarea class="textarea" placeholder="Descrição" v-model="fields.description"></textarea>
-                            </div>
-                        </div>
-                        <div class="field">
-                            <label class="label">Tipo</label>
-                            <div class="control">
-                                <div class="select">
-                                    <select name="type" v-model="fields.type">
-                                        <option value="">Selecione um tipo</option>
-                                        <option v-for="type in types" v-bind:key="type.id" :value="type.id">{{ type.name }}</option>
-                                    </select>
+                        <form id="add-volcano" @submit.prevent="addVolcano()" enctype="multipart/form-data">
+                            <div class="field">
+                                <label class="label">Nome</label>
+                                <div class="control">
+                                    <input class="input" type="text" placeholder="Nome" v-model="fields.name">
                                 </div>
                             </div>
-                        </div>
+                            <div class="field">
+                                <label class="label">Descrição</label>
+                                <div class="control">
+                                    <textarea class="textarea" placeholder="Descrição" v-model="fields.description"></textarea>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="label">Tipo</label>
+                                <div class="control">
+                                    <div class="select">
+                                        <select name="type" v-model="fields.type">
+                                            <option value="">Selecione um tipo</option>
+                                            <option v-for="type in types" v-bind:key="type.id" :value="type.id">{{ type.name }}</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="field img-field">
+                                <label class="label">Imagem</label>
+                                <div class="file has-name is-boxed">
+                                    <label class="file-label">
+                                        <input class="file-input" type="file" ref="img" @change="selectFile">
+                                        <span class="file-cta">
+                                        <span class="file-icon">
+                                            <i class="fas fa-upload"></i>
+                                        </span>
+                                        <span class="file-label">
+                                           Enviar imagem...
+                                        </span>
+                                        </span>
+                                        <span class="file-name">
+                                            Nenhum arquivo selecionado...
+                                        </span>
+                                    </label>
+                                </div>
+                            </div>  
+                        </form>
                     </section>
                     <footer class="modal-card-foot">
-                        <button class="button is-success" @click="addVolcano">Salvar</button>
+                        <button class="button is-success" type="submit" form="add-volcano">Salvar</button>
                         <button class="button" @click="closeAddVolcanoes">Cancelar</button>
                     </footer>
                   </div>
@@ -99,6 +120,7 @@ export default {
                 name: '',
                 description: '',
                 type: '',
+                img: null
             },
             active: '',
             hasError: false,
@@ -130,8 +152,18 @@ export default {
 
         async addVolcano() {
             try {
-                const qs = require('qs');
-                let response = await axios.post('http://192.168.0.110:3000/volcanoes',  qs.stringify(this.fields))
+                // const qs = require('qs');
+                // let response = await axios.post('http://192.168.0.110:3000/volcanoes',  qs.stringify(this.fields))
+                const params = this.setFormData(this.fields)
+              
+                const response = await axios.post('http://192.168.0.110:3000/volcanoes', params, 
+                {
+                    header: {
+                        "Content-Type": `multipart/form-data; boundary=${params._boundary}`,
+                    },
+                })
+                
+
                 this.volcanoes.push(response.data[0])
                 this.active = ''
                 this.hasError = false
@@ -143,13 +175,32 @@ export default {
             }
         },
 
+        setFormData(data) {
+            const formData = new FormData()
+          
+            for(let key in data) {
+                if(key === 'img')
+                    formData.append(key, data[key]) 
+                else
+                    formData.set(key, data[key])         
+            }
+        
+            return formData
+        },
+
         launchAddVolcanoes () {
             this.active = 'is-active'
         },
 
         closeAddVolcanoes() {
             this.active = ''
-        }
+        },
+
+        selectFile() {
+            const img = this.$refs.img.files[0];
+            this.fields.img = img
+        },
+
     }
 }
 </script>
@@ -163,6 +214,10 @@ export default {
     .add-button {
        font-size: 1rem;
        padding: 0px 1.2rem 0px 1.2rem;
+    }
+
+    .img-field {
+        margin-bottom: 1.5rem;
     }
 
 </style>
